d74851954a812be253a8925dc14aaf27259c9b5a
jay <jeanalainpro2@gmail.com>
2023-06-26 19:21:01 +0200

    ctrlc handler

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 pub mod csv_row_processor;
+use ctrlc;
 extern crate csv;
 use std::io;
 use std::process;
@@ -7,6 +8,8 @@ use chrono::NaiveDate;
 use colored::Colorize;
 use chrono::DateTime;
 use std::env;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
 
 
 
@@ -16,19 +19,29 @@ use std::env;
 
 struct CsvRow {
     row: csv::StringRecord,
-    new_col: String
+    label: String
 }
 
 
 impl CsvRow{
-    fn new(row: csv::StringRecord, new_col: String) -> Self {
-        CsvRow { row, new_col }
+    fn new(row: csv::StringRecord, label: String) -> Self {
+        CsvRow { row, label }
     }
 }
 
 
 fn main() {
     env::set_var("RUST_BACKTRACE", "1");
+    let running = Arc::new(AtomicBool::new(true));
+    let running_clone = running.clone();
+
+    ctrlc::set_handler(move ||{
+        if running_clone.load( Ordering::SeqCst){
+            running_clone.store(false,Ordering::SeqCst)
+        }else{
+            process::exit(0);
+        }
+    } ).expect("Error setting ctrl c handler");
     //-----------------------------------
     // Load table
     let csv_path="src/.data/processed_df_labelisation.csv";
@@ -90,7 +103,7 @@ fn main() {
         // 1---------------------------
         //  Detect anomaly
 
-        let collected_date = match NaiveDate::parse_from_str(collected_date_str, "%Y-%m-%d") {
+        let collected_date = match NaiveDate::parse_from_str(record.get(13).unwrap(), "%Y-%m-%d") {
             Ok(date) => date,
             Err(err) => {
                 eprintln!("Failed to parse collected_date: {}", err);
@@ -131,39 +144,60 @@ fn main() {
         //3---------------------------
         //print Information
 
-        let third_element=&record.get(2).unwrap_or("");
-        println!("Third element: {}", third_element.on_yellow());
-        println!("------------------------------------");
-        println!(" Original col :\n {:?}", record);
+        // println!(" Original col :\n {:?}", record);
         let mut new_col = String::new();
-        println!("Enter the new column value for row {:?}", record);
+        println!("Enter the label value for row " );
         io::stdin()
             .read_line(&mut new_col)
             .expect("Failed to read input");
         let new_col = new_col.trim().to_string();
+        let _new_col_clone=new_col.clone();
         let csv_row=CsvRow::new(record,new_col);
+        let last_row=csv_row.row.clone();
+
         rows.push(csv_row);
+        write_record(&mut wrt, &last_row,_new_col_clone);
+        println!("WRITTEN");
+        println!("LINE COUNTER: --- ");
+        if !running.load(Ordering::SeqCst){
+            println!("break");
+            break;
+            
+        }
+
+
     }
+    // if !running.load(Ordering::SeqCst){
+    //     println!("exit process");
+    //     break;
+    //     
+    // }
 
     //------------------------------------
     // Write outputs
 
     
-    for row in rows{
-        let mut record=row.row.clone();
-        record.push_field(&row.new_col);
-        let res=match wrt.write_record(&record) {
-            Ok(res)=> res,
-            Err(err) => {
-                eprintln!("Failed to create output file: {}", err);
-                process::exit(1);
-            }
-        };
-
-    }
+    // for row in rows{
+    //     let mut record=row.row.clone();
+    //     record.push_field(&row.label);
+    //     let res=match wrt.write_record(&record) {
+    //         Ok(res)=> res,
+    //         Err(err) => {
+    //             eprintln!("Failed to create output file: {}", err);
+    //             process::exit(1);
+    //         }
+    //     };
+    //
+    // }
 
 
 
     
     
 }
+
+    fn write_record(wrt: &mut csv::Writer<std::fs::File>, row: &csv::StringRecord,label:String) -> Result<(), csv::Error> {
+        let mut record = row.clone();
+        record.push_field(&label);
+        wrt.write_record(&record)
+ }
